a7961b6534aec8c2df33c0c37f8d8211e0b129bc
bruno <brunolemos88@gmail.com>
2014-05-10 21:57:44 -0300

    ModalidadeServlet e modalidades e novas imagens

--- Academia/WebContent/paginas/modalidades.jsp
+++ Academia/WebContent/paginas/modalidades.jsp
@@ -1,14 +1,40 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <jsp:useBean id="modalidadeBean"
 	class="br.edu.ifms.lp4.bean.ModalidadeBean"></jsp:useBean>
-<h2>Modalidades</h2>
-<a href="?p=cadastraModalidade" class="btn btn-small">Cadastrar
-	Modalidade</a>
 
-<hr>
+<c:if test="${not empty param['resposta']}">
+	<div class="${param['classResposta']}">
+		<strong>${param['resposta']}</strong>
+	</div>
+</c:if>
 
+<h4>Cadastro Modalidade</h4>
+<div class="row-fluid">
+	<div class="span12">
+
+		<form name="cadastra_modalidade" action="modalidade" method="get">
+			<fieldset>
+
+				<label>Descrição</label> <input class="input-block-level"
+					type="text" name="descricao" autofocus="autofocus" maxlength="50"
+					required="required" value="">
+
+				<div class="form-actions">
+					<button type="submit" name="enviar" class="btn btn-primary">Cadastrar</button>
+					<button type="reset" class="btn">Limpar</button>
+				</div>
+
+			</fieldset>
+		</form>
+
+	</div>
+</div>
+
+<h4>Lista Modalidades</h4>
 <c:if test="${empty modalidadeBean.modalidades}">
-	<span class="alert alert-error">Nenhuma Modalidade Cadastrada</span>
+	<div class='alert alert-error'>
+		<strong>Nenhuma Modalidade Cadastrada</strong>
+	</div>
 </c:if>
 
 <c:if test="${not empty modalidadeBean.modalidades}">
@@ -17,13 +43,17 @@
 			<tr class="bg-tabela-paginas">
 				<th width="40px">ID</th>
 				<th>Descrição</th>
+				<th width="90px">Funções</th>
 			</tr>
 		</thead>
 
-		<c:forEach var="madalidade" items="${modalidadeBean.modalidades}">
+		<c:forEach var="modalidade" items="${modalidadeBean.modalidades}">
 			<tr>
 				<td>${modalidade.id}</td>
 				<td>${modalidade.descricao}</td>
+				<td><a href="modalidade?id=${modalidade.id}" title="Apagar"
+					class="btn btn-mini btn-danger"><i
+						class="icon-remove icon-white"></i></a></td>
 			</tr>
 		</c:forEach>
 
--- Academia/src/br/edu/ifms/lp4/service/ModalidadeServlet.java
+++ Academia/src/br/edu/ifms/lp4/service/ModalidadeServlet.java
@@ -1,14 +1,12 @@
 package br.edu.ifms.lp4.service;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.edu.ifms.lp4.bean.ModalidadeBean;
 import br.edu.ifms.lp4.jpa.dao.ModalidadeDao;
 import br.edu.ifms.lp4.modelo.Modalidade;
 
@@ -16,39 +14,56 @@ import br.edu.ifms.lp4.modelo.Modalidade;
 public class ModalidadeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private String resposta = "";
+	private String classResposta = "";
+
 	public ModalidadeServlet() {
 		super();
+	}
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		ModalidadeDao modalidadeDao = new ModalidadeDao();
+
+		modalidadeDao.remove(modalidadeDao.recupera(id));
+
+		resposta = "Removido com sucesso!";
+		classResposta = "alert alert-success";
+
+		response.sendRedirect("index.jsp?pagina=modalidades&resposta="
+				+ resposta + "&classResposta=" + classResposta);
+
 	}
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		// String descricao = request.getParameter("descricao");
-		// String resposta = "";
-		//
-		// if (descricao != null && !descricao.isEmpty()) {
-		// Modalidade modalidade = new Modalidade();
-		// modalidade.setDescricao(descricao);
-		//
-		// ModalidadeBean modalidadeBean = new ModalidadeBean();
-		// resposta = modalidadeBean.salvaModalidade(modalidade) ?
-		// "Salvo com sucesso!" : "Erro ao gravar";
-		// }else{
-		// resposta = "Nenhuma descrição informada";
-		// }
-		//
-		// response.sendRedirect("index.jsp?resposta=" + resposta);
-	}
+		String descricao = request.getParameter("descricao");
 
-	@Override
-	protected void service(HttpServletRequest arg0, HttpServletResponse response)
-			throws ServletException, IOException {
-		super.service(arg0, response);
+		if (!descricao.isEmpty()) {
+			try {
+				Modalidade modalidade = new Modalidade();
 
-		ModalidadeBean modalidadeBean = new ModalidadeBean();
-		Modalidade modalidade = modalidadeBean.recuperaModalidade(1);
-	
-		response.sendRedirect("index.jsp?resposta=" + modalidadeBean.deletaModalidade(modalidade));
+				modalidade.setDescricao(descricao);
+				ModalidadeDao modalidadeDao = new ModalidadeDao();
+				modalidadeDao.salva(modalidade);
+
+				resposta = "Salvo com sucesso!";
+				classResposta = "alert alert-success";
+			} catch (Exception e) {
+				e.printStackTrace();
+				classResposta = "alert alert-error";
+				resposta = "Ocorreu um erro ao tentar realizar o cadastro.";
+			}
+		} else {
+			classResposta = "alert alert-error";
+			resposta = "Dados incompletos!";
+		}
+		response.sendRedirect("index.jsp?pagina=modalidades&resposta="
+				+ resposta + "&classResposta=" + classResposta);
 	}
 
 }
