cd4d3efaf05c52520564256bf64a9538d8646c3b
mateus <nicolasmateus@ymail.com>
2014-05-11 20:18:42 -0400

    Commit equipamento

--- Academia/src/br/edu/ifms/lp4/bean/EquipamentoBean.java
+++ Academia/src/br/edu/ifms/lp4/bean/EquipamentoBean.java
@@ -1,5 +1,6 @@
 package br.edu.ifms.lp4.bean;
 
+import java.util.HashMap;
 import java.util.List;
 
 import br.edu.ifms.lp4.jpa.dao.EquipamentoDao;
@@ -10,21 +11,68 @@ public class EquipamentoBean {
 	private List<Equipamento> equipamentos;
 	private EquipamentoDao equipamentoDao = new EquipamentoDao();
 
+	private final String classeCSSSucesso = "alert alert-success";
+	private final String classeCSSErro = "alert alert-error";
+	private final String mensagem = "mensagem";
+	private final String classeCSS = "classeCSS";
+
 	public List<Equipamento> getEquipamentos() {
 		equipamentos = equipamentoDao.recuperaTodos();
 		return equipamentos;
 	}
 
-	public boolean salvaEquipamento(Equipamento equipamento) {
-		return equipamentoDao.salva(equipamento) != null ? true : false;
+	public HashMap<String, String> salvaEquipamento(Equipamento equipamento) {
+
+		if (equipamento == null || equipamento.getDescricao().equals("")) {
+			return preencheMensagens(classeCSSErro,
+					"Descrição deve ser preenchida!");
+		} else {
+			if (equipamentoDao.salva(equipamento) != null) {
+				return preencheMensagens(classeCSSSucesso,
+						"Equipamento salva com sucesso!");
+			}
+		}
+
+		return preencheMensagens(classeCSSErro, "Error ao salvar equipamento!");
 	}
 
-	public boolean deletaEquipamento(Equipamento equipamento) {
-		return equipamentoDao.remove(equipamento);
+	public HashMap<String, String> removeEquipamento(Integer id) {
+		Equipamento equipamento = recuperaEquipamento(id);
+
+		if (equipamento == null) {
+			preencheMensagens(classeCSSErro, "Equipamento não encontrado!");
+		} else {
+			if (equipamentoDao.remove(equipamento)) {
+				return preencheMensagens(classeCSSSucesso,
+						"Equipamento removido com sucesso!");
+			}
+		}
+		return preencheMensagens(classeCSSErro, "Error ao remover equipamento!");
 	}
-	
-	public Equipamento recuperaEquipamento(Integer id){
+
+	public Equipamento recuperaEquipamento(Integer id) {
 		return equipamentoDao.recupera(id);
 	}
-	
+
+	private HashMap<String, String> preencheMensagens(String classeCSS,
+			String mensagem) {
+		if (classeCSS.equals(classeCSSErro)) {
+			HashMap<String, String> mensagens = new HashMap<>();
+			mensagens.put(this.classeCSS, classeCSSErro);
+			mensagens.put(this.mensagem, mensagem);
+			return mensagens;
+		}
+		if (classeCSS.equals(classeCSSSucesso)) {
+			HashMap<String, String> mensagens = new HashMap<>();
+			mensagens.put(this.classeCSS, classeCSSSucesso);
+			mensagens.put(this.mensagem, mensagem);
+			return mensagens;
+		}
+		HashMap<String, String> mensagens = new HashMap<>();
+		mensagens.put(classeCSS, classeCSSErro);
+		mensagens.put(mensagem, "SEGUINTE FUDEU, MUITO. Você chegou até aqui!");
+		return mensagens;
+
+	}
+
 }
